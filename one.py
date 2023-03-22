25100922409faf4608d5851a54d33fddc314880d
Paige Rackley <102173123+paigerackley@users.noreply.github.com>
2023-03-22 11:31:35 -0500

    __name__ and '__main__'

---
+++ one.py
@@ -0,0 +1,16 @@
+#one.py
+
+def func():
+	print('FUNC() IN ONE.PY')
+
+def function():
+	pass
+
+def function2():
+	pass
+
+
+if __name__ == '__main__':
+	# RUN THE SCRIPT!
+	function2()
+	function()
