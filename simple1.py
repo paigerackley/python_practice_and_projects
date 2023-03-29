3cd95623c341a58c43ac5b143d9219b021dedeac
Paige Rackley <102173123+paigerackley@users.noreply.github.com>
2023-03-29 13:38:52 -0500

    White space only/ pylint grade of 8.33

--- simple1.py
+++ simple1.py
@@ -1,4 +1,14 @@
-a = 1
-b = 2
-print(a)
-print(B)
+'''
+A Very Simple Script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
